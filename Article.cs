75c68e84006a51c973d2ec1a16320a9c89f3c2dd
unknown <TiW@LT-TIW1.dk.sitecore.net>
2013-05-29 14:48:42 +0200

    Changed Articel Class

--- Article.cs
+++ Article.cs
@@ -6,12 +6,20 @@ using System.Web;
 namespace seven
 {
     using Sitecore.ContentSearch;
+    using Sitecore.Data;
 
-    public class Article
+    public class Article //: SearchResultItem
     {
         public string Title { get; set; }
 
         [IndexField("__smallcreateddate")]
         public DateTime PublishDate { get; set; }
+
+        [IndexField(BuiltinFields.DataSource)]
+        public string Datasource { get; set; }
+
+        [IndexField(BuiltinFields.ID)]
+        public ID ArticleId { get; set; }
+
     }
 }
