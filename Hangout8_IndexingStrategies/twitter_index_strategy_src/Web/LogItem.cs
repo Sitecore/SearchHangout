d1601367120f5c283483039e77836b8aba1ee4ff
sitecorian <alex.shyba@gmail.com>
2013-10-30 10:36:41 -0700

    demo code for twitter based index update strategy

---
+++ Hangout8_IndexingStrategies/twitter_index_strategy_src/Web/LogItem.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.Data;
+using Sitecore.Events;
+using System;
+using Sitecore.StringExtensions;
+using Sitecore.ContentSearch;
+using Sitecore.Diagnostics;
+using Sitecore.Data.Events;
+using Sitecore.Data.Items;
+
+namespace IndexStrategySandBox
+{
+    public class LogItem
+    {
+        private const string Prefix = "--IndexDemo--";
+
+        public void ItemUpdated(object sender, EventArgs args)
+        {
+            var uri = Event.ExtractParameter<SitecoreItemUniqueId>(args, 1);
+            var item = Database.GetItem(uri);
+            Log.Info("{0} Item updated in index: '{1}' ({2})".FormatWith(Prefix, item.Name, item.Uri), this);
+        }
+
+        public void ItemCreated(object sender, EventArgs args)
+        {
+            var targs = Event.ExtractParameter<ItemCreatedEventArgs>(args, 0);
+            Log.Info("{0} Item created: {1} ({2})".FormatWith(Prefix, targs.Item.Name, targs.Item.Uri), this);
+        }
+
+        public void ItemSaved(object sender, EventArgs args)
+        {
+            var item = Event.ExtractParameter<Item>(args, 0);
+            Log.Info("{0} Item saved: {1} ({2})".FormatWith(Prefix, item.Name, item.Uri), this);
+        }
+    }
+}
---
+++ Hangout8_IndexingStrategies/twitter_index_strategy_src/Web/TwitterUpdateStrategy.cs
@@ -0,0 +1,73 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.ContentSearch.Maintenance.Strategies;
+using Sitecore.Diagnostics;
+using Sitecore.StringExtensions;
+using Streaminvi;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetinCore.Interfaces;
+using TweetinCore.Interfaces.TwitterToken;
+using TwitterToken;
+
+namespace IndexStrategySandBox
+{
+    public class TwitterUpdateStrategy : IIndexUpdateStrategy
+    {
+        private ISearchIndex m_index = null;
+
+        public string HashtagTrigger { get; set; }
+
+        public void Initialize(ISearchIndex searchIndex)
+        {
+            // Store index
+            m_index = searchIndex;
+
+            // Create Twitter token
+            var token = new Token(
+                ConfigurationManager.AppSettings["accessToken"],
+                ConfigurationManager.AppSettings["accessTokenSecret"],
+                ConfigurationManager.AppSettings["consumerKey"],
+                ConfigurationManager.AppSettings["consumerSecret"]);
+
+            // Start twitter streaming thread
+            var task = new Task(() => StartTwitterStream(token));
+            task.Start();
+        }
+
+        async void StartTwitterStream(IToken token)
+        {
+            var stream = new FilteredStream();
+
+            CrawlingLog.Log.Info("Init TwitterUpdateStrategy with Hashtag '{0}'".FormatWith(HashtagTrigger));
+            Log.Info("Init TwitterUpdateStrategy with Hashtag '{0}'".FormatWith(HashtagTrigger), this);
+
+            stream.AddTrack(HashtagTrigger);
+
+            stream.StreamStarted += (sender, args) => CrawlingLog.Log.Info("Twitter stream started");
+
+            // The following call blocks
+            stream.StartStream(token, x => HandleTweet(x));
+        }
+
+        void HandleTweet(ITweet tweet)
+        {
+            if ((from tag in tweet.Hashtags where tag.Text == HashtagTrigger select tag).Any())
+            {
+                Log.Info("Index rebuild triggered from Twitter by '{0}'".FormatWith(tweet.Creator.Name), this);
+                CrawlingLog.Log.Info("Index rebuild triggered from Twitter by '{0}'".FormatWith(tweet.Creator.Name));
+
+                if (IndexCustodian.IsIndexingPaused(m_index) || IndexCustodian.IsRebuilding(m_index))
+                {
+                    CrawlingLog.Log.Warn("Indexing call muted");
+                    Log.Info("Indexing call muted", this);
+                    return;
+                }
+
+                IndexCustodian.FullRebuild(m_index);
+            }
+        }
+    }
+}
